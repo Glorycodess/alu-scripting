3e45667f1f1605f4dffeb2ef316f2b8561c682cb
Glorycodess <g.paul@alustudent.com>
2024-09-05 17:26:50 -0700

    8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #Glorycodes herei again
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flag:(.*?)\]/).join(',")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flag:(.*?)\]/).join(",")
